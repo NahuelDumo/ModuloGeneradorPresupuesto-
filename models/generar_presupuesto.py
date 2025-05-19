a7466778183bd89d39076bb5ba70ebfbcd50bf9a
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-05-19 19:24:03 -0300

    Ajuste

--- models/generar_presupuesto.py
+++ models/generar_presupuesto.py
@@ -36,9 +36,10 @@ class SaleOrder(models.Model):
             # Datos necesarios para el PDF
             nombre_cliente = f"<span style='font-family: Roboto, sans-serif; word-spacing: 0px;'>{record.partner_id.name or '-'}</span>"
 
-
-            contacto = record.partner_id.parent_id.name or "-"
-            if 22 > len(contacto) > 18:
+            if len(contacto) < 18:
+                contacto = f"<span style='font-family: Roboto, sans-serif ;font-size: 95px;'>{(contacto)}</span>"
+   
+            elif 22 > len(contacto) > 18:
                 contacto = f"<span style='font-family: Roboto, sans-serif ;font-size: 85px;'>{(contacto)}</span>"
             elif 25>len(contacto) > 22:
                 contacto = f"<span style='font-family: Roboto, sans-serif ;font-size: 75px;'>{(contacto)}</span>"
