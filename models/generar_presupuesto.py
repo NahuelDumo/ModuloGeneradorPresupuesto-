b16eaaaec4503a674db41eb45d2cbf90667a0afa
Nahuel <nahueldumo2014@gmail.com>
2026-07-18 19:26:24 -0300

    feat: add budget generation model and static website template for update services

--- models/generar_presupuesto.py
+++ models/generar_presupuesto.py
@@ -270,7 +270,7 @@ class SaleOrder(models.Model):
             html_content = re.sub(
                 r'<div class="t m0 x5 ha (y18|yf) ff2 fs8 fc3 sc0 lsb ws7">Valor Cuota:<span class="_ _3"></span><span class="fs9 lsa">(En 2 pagos [^<]*)<span class="_ _4"> </span>Valor total: \{\{total_1\}\}<span class="_ _5"></span><span class="ff3 fs8 lsb ws3">\{\{valor_cuota1\}\}</span></span></div>',
                 lambda m: (
-                    r'<div class="t m0 hc ' + m.group(1) + r' ff2 fs9 fc3 sc0 ls2 ws7" style="left: 310px;">' + m.group(2) + r'</div>\n' +
+                    r'<div class="t m0 hc ' + m.group(1) + r' ff2 fs9 fc3 sc0 ls2 ws7" style="left: 330px;">' + m.group(2) + r'</div>\n' +
                     r'<div class="t m0 hc ' + m.group(1) + r' ff2 fs9 fc3 sc0 ls2 ws7" style="left: 490px;">{{valor_cuota1_overlay}}</div>\n' +
                     r'<div class="t m0 hc ' + m.group(1) + r' ff2 fs9 fc3 sc0 ls2 ws7" style="left: 670px;">{{total_1}}</div>'
                 ),
